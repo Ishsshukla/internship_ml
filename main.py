cea246952eb9be0c8e20134eab2b96ff53125be6
ishika <shukla22164024@akgec.ac.in>
2023-11-18 14:08:25 +0530

    cors

--- main.py
+++ main.py
@@ -7,10 +7,21 @@ from fastapi import FastAPI
 import requests
 
 app = FastAPI()
-
 # CORS (Cross-Origin Resource Sharing) settings
-origins = ["http://127.0.0.1:8000/", "http://localhost:5173", "https://intrship.onrender.com/", "http://localhost:5000","https://workshala-navy.vercel.app/"]
-app.add_middleware(CORSMiddleware, allow_origins=origins, allow_credentials=True, allow_methods=["*"], allow_headers=["*"])
+origins = [
+    "http://127.0.0.1:8000",
+    "http://localhost:5173",
+    "https://workshala-navy.vercel.app",
+    "https://intrship.onrender.com",
+    "http://localhost:5000",
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 response = requests.get('https://workshala-7v7q.onrender.com/internshipData')
